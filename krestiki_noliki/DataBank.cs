85a16626727e4ce6512a968ab984e33f8c4e56a5
IG0R_Penza <igorkovalev2006@gmail.com>
2024-10-19 13:08:46 +0300

    zero commit

---
+++ krestiki_noliki/DataBank.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace krestiki_noliki
+{
+    static class DataBank
+    {
+        public static string[,] Field = { 
+            { "", "", "" }, 
+            { "", "", "" }, 
+            { "", "", "" } };
+        public static int Move = 0;
+        public static string[] Figure = { "O", "X" };
+    }
+}
---
+++ krestiki_noliki/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace krestiki_noliki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button00 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button01 = new System.Windows.Forms.Button();
+            this.button02 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button22 = new System.Windows.Forms.Button();
+            this.message = new System.Windows.Forms.Label();
+            this.restart_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button00
+            // 
+            this.button00.BackColor = System.Drawing.SystemColors.Control;
+            this.button00.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button00.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button00.FlatAppearance.BorderSize = 5;
+            this.button00.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.button00.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button00.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button00.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button00.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button00.Location = new System.Drawing.Point(21, 26);
+            this.button00.Name = "button00";
+            this.button00.Size = new System.Drawing.Size(90, 90);
+            this.button00.TabIndex = 0;
+            this.button00.UseVisualStyleBackColor = false;
+            //this.button00.Click += new System.EventHandler(this.button00_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(21, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 10);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(18, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(300, 10);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(21, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 300);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(275, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 300);
+            this.label4.TabIndex = 4;
+            // 
+            // button01
+            // 
+            this.button01.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button01.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button01.FlatAppearance.BorderSize = 5;
+            this.button01.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.button01.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button01.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button01.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button01.Location = new System.Drawing.Point(108, 26);
+            this.button01.Name = "button01";
+            this.button01.Size = new System.Drawing.Size(90, 90);
+            this.button01.TabIndex = 5;
+            this.button01.UseVisualStyleBackColor = true;
+            //this.button01.Click += new System.EventHandler(this.button01_Click);
+            // 
+            // button02
+            // 
+            this.button02.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button02.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button02.FlatAppearance.BorderSize = 5;
+            this.button02.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.button02.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button02.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button02.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button02.Location = new System.Drawing.Point(195, 26);
+            this.button02.Name = "button02";
+            this.button02.Size = new System.Drawing.Size(90, 90);
+            this.button02.TabIndex = 6;
+            this.button02.UseVisualStyleBackColor = true;
+            //this.button02.Click += new System.EventHandler(this.button02_Click);
+            // 
+            // button10
+            // 
+            this.button10.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button10.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button10.FlatAppearance.BorderSize = 5;
+            this.button10.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.button10.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button10.Location = new System.Drawing.Point(21, 110);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(90, 90);
+            this.button10.TabIndex = 7;
+            this.button10.UseVisualStyleBackColor = true;
+            //this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button11.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button11.FlatAppearance.BorderSize = 5;
+            this.button11.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.button11.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button11.Location = new System.Drawing.Point(108, 110);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(90, 90);
+            this.button11.TabIndex = 8;
+            this.button11.UseVisualStyleBackColor = true;
+            //this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button12.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button12.FlatAppearance.BorderSize = 5;
+            this.button12.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.button12.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button12.Location = new System.Drawing.Point(195, 110);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(90, 90);
+            this.button12.TabIndex = 9;
+            this.button12.UseVisualStyleBackColor = true;
+            //this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button20
+            // 
+            this.button20.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button20.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button20.FlatAppearance.BorderSize = 5;
+            this.button20.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.button20.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button20.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button20.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button20.Location = new System.Drawing.Point(21, 194);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(90, 90);
+            this.button20.TabIndex = 10;
+            this.button20.UseVisualStyleBackColor = true;
+            //this.button20.Click += new System.EventHandler(this.button20_Click);
+            // 
+            // button21
+            // 
+            this.button21.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button21.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button21.FlatAppearance.BorderSize = 5;
+            this.button21.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.button21.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button21.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button21.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button21.Location = new System.Drawing.Point(108, 194);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(90, 90);
+            this.button21.TabIndex = 11;
+            this.button21.UseVisualStyleBackColor = true;
+            //this.button21.Click += new System.EventHandler(this.button21_Click);
+            // 
+            // button22
+            // 
+            this.button22.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button22.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button22.FlatAppearance.BorderSize = 5;
+            this.button22.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.button22.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button22.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button22.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button22.Location = new System.Drawing.Point(195, 194);
+            this.button22.Name = "button22";
+            this.button22.Size = new System.Drawing.Size(90, 90);
+            this.button22.TabIndex = 12;
+            this.button22.UseVisualStyleBackColor = true;
+            //this.button22.Click += new System.EventHandler(this.button22_Click);
+            // 
+            // message
+            // 
+            this.message.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.message.Location = new System.Drawing.Point(344, 51);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(271, 43);
+            this.message.TabIndex = 13;
+            this.message.Text = "Ходит:  O";
+            // 
+            // restart_btn
+            // 
+            this.restart_btn.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.restart_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.restart_btn.Enabled = false;
+            this.restart_btn.FlatAppearance.BorderColor = System.Drawing.Color.Tan;
+            this.restart_btn.FlatAppearance.BorderSize = 3;
+            this.restart_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.restart_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.restart_btn.Location = new System.Drawing.Point(351, 162);
+            this.restart_btn.Name = "restart_btn";
+            this.restart_btn.Size = new System.Drawing.Size(226, 81);
+            this.restart_btn.TabIndex = 14;
+            this.restart_btn.Text = "RESTART";
+            this.restart_btn.UseVisualStyleBackColor = false;
+            this.restart_btn.Visible = false;
+            //this.restart_btn.Click += new System.EventHandler(this.restart_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 315);
+            this.Controls.Add(this.restart_btn);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button00);
+            this.Controls.Add(this.button01);
+            this.Controls.Add(this.button02);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button22);
+            this.Controls.Add(this.button21);
+            this.Controls.Add(this.button20);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Крестики-нолики";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button00;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button01;
+        private System.Windows.Forms.Button button02;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button22;
+        private System.Windows.Forms.Label message;
+        private System.Windows.Forms.Button restart_btn;
+    }
+}
+
